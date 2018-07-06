613711a9209332292b558fa8d4a035d073fdb15b
Jia Lin <v-jilin2@microsoft.com>
2018-07-06 20:32:15 +0800

    1

---
+++ JekyllNote/JekyllNote/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JekyllNote
+//
+//  Created by LinJia on 2018/7/5.
+//  Copyright © 2018年 chongxiaoge. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JekyllNote/JekyllNote/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JekyllNote
+//
+//  Created by LinJia on 2018/7/5.
+//  Copyright © 2018年 chongxiaoge. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
